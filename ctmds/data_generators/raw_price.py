5a9c4727a385c4e9ee98059e0bbed90c0bfb3ae1
Victor Sandoval <vs.software.eng@gmail.com>
2025-02-09 21:15:50 +0000

    several changes: - chore: move 'data_generators' module out of 'domain' module - feat: add base_price argument to 'daily_prices_with_timestamps()' generator

--- ctmds/data_generators/raw_price.py
+++ ctmds/data_generators/raw_price.py
@@ -17,7 +17,7 @@ def random_generator(num: int) -> np.ndarray:
 def normal_distribution_generator(
     base_price: float,
     periods: int,
-    std_dev: float = 2.0,
+    std_dev: float = 1.0,
     seed: int | None = None,
 ) -> list[float]:
     """Generate random decimals representing prices, following a normal distribution"""
